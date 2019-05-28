b5e3971ce21dbeb0a70f126e48910533e0b5db06
pengjiyuan <pengjiyuan@bytedance.com>
2019-05-28 21:07:39 +0800

    update template

--- templates/code_cell/index.jsx
+++ templates/code_cell/index.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { IconCode, IconDesign } from '@byted/byteui/icon';
 
-export default function CodeCell(props) {
-  return (
-    <div className="code">{props.children}</div>
-  );
+class CodeCell extends React.PureComponent {
+  state = {
+    codeOpen: false
+  }
+
+  toggleCode = () => {
+    this.setState({
+      codeOpen: !this.state.codeOpen
+    });
+  }
+
+  render() {
+    const props = this.props;
+    const { codeOpen } = this.state;
+    return (
+      <div className={`code ${codeOpen ? '' : 'hide'}`}>
+        <div className="select-bar">
+          <button className="doc_design disabled"><IconDesign />查看交互说明</button>
+          <button onClick={this.toggleCode} className="doc_code"><IconCode />查看代码</button>
+        </div>
+        {props.children}
+      </div>
+    );
+  }
 }
+
+export default CodeCell;
---
+++ templates/description_cell/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function DescriptionCell(props) {
+  return (
+    <div className="description">{props.children}</div>
+  );
+}
