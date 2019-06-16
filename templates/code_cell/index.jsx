232c8b094cd0c900ba6c4d9884703b4d8968ff5b
pengjiyuan <pengjiyuan@bytedance.com>
2019-06-16 18:22:19 +0800

    feat: support design block

--- templates/code_cell/index.jsx
+++ templates/code_cell/index.jsx
@@ -3,25 +3,43 @@ import { IconCode, IconInteraction } from '@byted/byteui/icon';
 
 class CodeCell extends React.PureComponent {
   state = {
-    codeOpen: false
+    codeOpen: false,
+    designOpen: false
   }
 
   toggleCode = () => {
     this.setState({
+      designOpen: false,
       codeOpen: !this.state.codeOpen
+    });
+  }
+
+  toggleDesign = () => {
+    this.setState({
+      codeOpen: false,
+      designOpen: !this.state.designOpen
     });
   }
 
   render() {
     const props = this.props;
-    const { codeOpen } = this.state;
+    const { codeOpen, designOpen } = this.state;
     return (
       <div className={`code ${codeOpen ? '' : 'hide'}`}>
         <div className="select-bar">
-          <button className="doc_design disabled"><IconInteraction style={{ fontSize: 16 }} />查看交互说明</button>
-          <button onClick={this.toggleCode} className="doc_code"><IconCode style={{ fontSize: 16 }} />查看代码</button>
+          <button onClick={this.toggleDesign} className={`doc_design ${designOpen ? 'active' : ''}`}><IconInteraction style={{ fontSize: 16 }} />查看交互说明</button>
+          <button onClick={this.toggleCode} className={`doc_code ${codeOpen ? 'active' : ''}`}><IconCode style={{ fontSize: 16 }} />查看代码</button>
         </div>
-        {props.children}
+        <div className="content-code-design">
+          <div className={`design ${designOpen ? '' : 'hide'}`}>
+            <div className="design_wrapper">
+              {props.design || '暂无交互说明'}
+            </div>
+          </div>
+          <div className={`code ${codeOpen ? '' : 'hide'}`}>
+            {props.children}
+          </div>
+        </div>
       </div>
     );
   }
